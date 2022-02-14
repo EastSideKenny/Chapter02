befab2321f52ab06fb9feef41b8a418e71097460
Jerome Jacob <Jerome56jj@hotmail.com>
2022-02-14 21:28:03 +0100

    Add project files.

---
+++ Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿// unsigned integer means positive whole number or 0
+uint naturalNumber = 23;
+
+// integer means negative or positive whole number or 0
+int integerNumber = -23;
+
+// float means single-precision floating point
+// F suffix makes it a float literal
+float realNumber = 2.3F;
+
+// double means double-precision floating point
+double anotherRealNumber = 2.3; // double literal
