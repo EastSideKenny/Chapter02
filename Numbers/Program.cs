1b15a644bf2e6ce338444fc047197aaa8aac5f86
Jerome Jacob <Jerome56jj@hotmail.com>
2022-02-14 22:22:16 +0100

    experimenting with Variables

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -38,3 +38,16 @@ else
     Console.WriteLine($"{a} + {b} does NOT equal {0.3}");
 }
 
+Console.WriteLine("Using decimals:");
+decimal c = 0.1M; //M suffix means a decimal literal value
+decimal d = 0.2M;
+
+if (c + d == 0.3M)
+{
+    Console.WriteLine($"{c} + {d} equals {0.3}");
+}
+else
+{
+    Console.WriteLine($"{c} + {d} does NOT equal {0.3}");
+}
+
