c713527a8d77832eb093827d629a79026c84eafe
Jérôme JACOB <jerome56jj@hotmail.com>
2022-02-15 15:55:49 +0100

    Arguments project

--- Formatting/Program.cs
+++ Formatting/Program.cs
@@ -1,7 +1,9 @@
-﻿int numberOfApples = 12;
+﻿using static System.Console;
+
+int numberOfApples = 12;
 decimal pricePerApple = 0.35M;
 
-Console.WriteLine(
+WriteLine(
     format: "{0} apples costs {1:C}",
     arg0: numberOfApples,
     arg1: pricePerApple * numberOfApples);
@@ -13,9 +15,9 @@ string formatted = string.Format(
 
 //WriteToFile(formatted); //writes the string to a file
 
-Console.WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
+WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
 
-Console.WriteLine();
+WriteLine();
 
 string applesText = "Apples";
 int applesCount = 1234;
@@ -23,18 +25,36 @@ int applesCount = 1234;
 string bananasText = "Bananas";
 int bananasCount = 56789;
 
-Console.WriteLine(
+WriteLine(
     format: "{0,-10} {1,6:N0}",
     arg0: "Name",
     arg1: "Count");
 
-Console.WriteLine(
+WriteLine(
     format: "{0,-10} {1,6:N0}",
     arg0: applesText,
     arg1: applesCount);
 
 
-Console.WriteLine(
+WriteLine(
     format: "{0,-10} {1,6:N0}",
     arg0: bananasText,
     arg1: bananasCount);
+
+
+Write("Type your first name and press ENTER: ");
+string? firstName = ReadLine();
+
+Write("Type your age and press ENTER: ");
+string? age = ReadLine();
+
+WriteLine(
+    $"Hello {firstName}, you look good for {age}.");
+
+Write("Press any key combination: ");
+ConsoleKeyInfo key = ReadKey();
+WriteLine();
+WriteLine("Key: {0}, Char: {1}, Modifiers: {2}",
+    arg0: key.Key,
+    arg1: key.KeyChar,
+    arg2: key.Modifiers);
